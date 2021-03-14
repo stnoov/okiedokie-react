e3f580550597a206b1cb116bfce6ab124a8fe314
Artem Sitnov <stnoov@gmail.com>
2021-03-14 18:18:04 +0200

    add profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Profile.css'
+import {Container, Row, Col} from "react-bootstrap";
+import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
 
 const Profile = () => {
+
+
+
     return (
         <div className='geometry-bg profile-page'>
-
+            <Container>
+                <div className="profile-name">
+                    <h2>Artem Sitnov</h2>
+                </div>
+                <div className="profile-details">
+                    <div className="details-okie-points">
+                        Мои OkieDokie points: 0 <button className='swap-okie-btn'><SwapHorizIcon /> Обменять</button>
+                    </div>
+                    <div className="details-email">
+                        <span className="current-email">Почта: <input className="change-email" type="text" value='stnoov@gmail.com'/></span> <button className='change-email-btn'>Обновить</button>
+                    </div>
+                    <div className="update-password">
+                        Обновить пароль
+                        <input className='update-password-input' type="password" placeholder="Старый пароль"/>
+                        <input className='update-password-input' type="password" placeholder="Новый пароль"/>
+                        <button className='update-password-btn'>Обновить</button>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
