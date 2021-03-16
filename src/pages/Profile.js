0ce63b9ba53d8e386085de615c91a306e1cb4f98
Artem Sitnov <stnoov@gmail.com>
2021-03-16 18:59:12 +0200

    add history to Profile page, small ui/ux changes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/Profile.css'
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Table} from "react-bootstrap";
 import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import HistoryIcon from '@material-ui/icons/History';
@@ -8,30 +8,66 @@ import HistoryIcon from '@material-ui/icons/History';
 const Profile = () => {
 
 
-
+    const [activeTab, setActiveTab] = React.useState(0)
     return (
         <div className='geometry-bg profile-page'>
             <Container>
                 <div className="profile-header">
-                    <div className='profile-tab'><AccountCircleIcon fontSize={"large"}/>Профиль</div>
-                    <div className='history-tab'><HistoryIcon fontSize={"large"}/>История</div>
+                    <div className={`profile-tab ${activeTab === 0 ? 'active-tab' : ''}`} onClick={() => setActiveTab(0)}><AccountCircleIcon fontSize={"large"}/>Профиль</div>
+                    <div className={`history-tab ${activeTab === 1 ? 'active-tab' : ''}`} onClick={() => setActiveTab(1)}><HistoryIcon fontSize={"large"}/>История</div>
                 </div>
-                <div className="profile-details">
-                    <div className="details-okie-points">
-                        Мои OkieDokie points: 0 <button className='swap-okie-btn'><SwapHorizIcon /> Обменять</button>
+                {activeTab === 0 ?
+                    <div className="profile-details">
+                        <div className="details-okie-points">
+                            Мои OkieDokie points: 0 <button className='swap-okie-btn'><SwapHorizIcon /> Обменять</button>
+                        </div>
+                        <div className="details-email">
+                            <span className="current-email">Почта:</span>
+                            <input className="change-email" type="text" value='stnoov@gmail.com'/>
+                            <button className='change-email-btn'>Изменить</button>
+                        </div>
+                        <div className="update-password">
+                            Обновить пароль
+                            <input className='update-password-input' type="password" placeholder="Старый пароль"/>
+                            <input className='update-password-input' type="password" placeholder="Новый пароль"/>
+                            <button className='update-password-btn'>Обновить</button>
+                        </div>
                     </div>
-                    <div className="details-email">
-                        <span className="current-email">Почта:</span>
-                        <input className="change-email" type="text" value='stnoov@gmail.com'/>
-                        <button className='change-email-btn'>Изменить</button>
-                    </div>
-                    <div className="update-password">
-                        Обновить пароль
-                        <input className='update-password-input' type="password" placeholder="Старый пароль"/>
-                        <input className='update-password-input' type="password" placeholder="Новый пароль"/>
-                        <button className='update-password-btn'>Обновить</button>
-                    </div>
+                :
+                <div className='profile-history'>
+                    <Table striped responsive borderless hover variant="light">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Продукт</th>
+                            <th>Дата</th>
+                            <th>Статус</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>1 OC</td>
+                            <td>4 Марта</td>
+                            <td>Оплачено</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>5 OC</td>
+                            <td>4 Марта</td>
+                            <td>Оплачено</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>10 OC</td>
+                            <td>4 Марта</td>
+                            <td>Оплачено</td>
+                        </tr>
+                        </tbody>
+                    </Table>
                 </div>
+                }
+
             </Container>
         </div>
     );
